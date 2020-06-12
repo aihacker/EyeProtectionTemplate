95c2427a84b8a67ee53a0d0c2694fc9b3b31358b
aihacker <271671891@qq.com>
2020-06-12 11:58:16 +0800

    测试

--- EyeProtectedTemplate/Form1.cs
+++ EyeProtectedTemplate/Form1.cs
@@ -11,8 +11,13 @@ using System.Runtime.InteropServices;
 
 namespace EyeProtectedTemplate
 {
+    //https://blog.csdn.net/weixin_34268843/article/details/86156321
     public partial class Form1 : Form
     {
+        //碰见问题：git 一直报错
+        //          将分支推送到远程存储库时遇到错误: rejected Updates were rejected because the remote contains work that you do not have locally. This is usually caused by another repository pushing to the same ref. You may want to first integrate the remote changes before pushing again.
+        //解决方法：https://www.cnblogs.com/jimaojin/p/7667830.html
+        //          git pull origin master --allow-unrelated-histories
         public Form1()
         {
             InitializeComponent();
@@ -29,7 +34,8 @@ namespace EyeProtectedTemplate
         const int LWA_ALPHA = 2;
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.BackColor = Color.Silver;
+            //this.BackColor = Color.Silver;
+            this.BackColor = Color.FromArgb(204,232,207);//https://www.cnblogs.com/wfy680/p/12001306.html
             this.TopMost = true;
             this.FormBorderStyle = FormBorderStyle.None;
             this.WindowState = FormWindowState.Maximized;
